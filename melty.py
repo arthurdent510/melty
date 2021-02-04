43aeece65b1b4c28930de787d63a0eaf05a8c8f6
David Hurt <dave@mbpofdoooooooom.lan>
2021-02-04 09:19:44 -0700

    uploading correct code

--- melty.py
+++ melty.py
@@ -1,71 +1,27 @@
-import time
+import analogio
 import board
-import pulseio
-from digitalio import DigitalInOut, Direction, Pull
-from adafruit_motor import servo, motor
-
-
-in1 = DigitalInOut(board.D13)
-in1.direction = Direction.OUTPUT
-
-in2 = DigitalInOut(board.D12)
-in2.direction = Direction.OUTPUT
-
-pwm = pulseio.PWMOut(board.A1, frequency=50)
+import time
  
-# Create a servo object, my_servo.
-my_servo = servo.ContinuousServo(pwm)
-# foo = motor.DCMotor(pwm)
-
-
-leftStop = DigitalInOut(board.D2)
-leftStop.direction = Direction.INPUT
-leftStop.pull = Pull.UP
-
-rightStop = DigitalInOut(board.D3)
-rightStop.direction = Direction.INPUT
-rightStop.pull = Pull.UP
-
-# in1 and in2 to 0 is break
-# in1 and in2 to 1 is coast
-# in1 to 1 and in2 to 0, forward
-# in1 to 0 and in2 to 1, reverse
-
-my_servo.throttle = 0
-
-time.sleep(5)
-
-# my_servo.throttle = .5
-
-
-
-in1.value = True
-in2.value = False
-
-direction = False
-
+ 
+# Create analog inputs for each ADXL335 axis.
+# x_axis = analogio.AnalogIn(board.A1)
+# y_axis = analogio.AnalogIn(board.A2)
+z_axis = analogio.AnalogIn(board.A2)
+ 
+# Define function to convert raw analog values to gravities.
+def accel_value(axis):
+    # Convert axis value to float within 0...1 range.
+    val = axis.value / 65535
+    # Shift values to true center (0.5).
+    val -= 0.5
+    # Convert to gravities.
+    return val * 3.0
+ 
+# Main loop prints acceleration every second.
 while True:
-    if rightStop.value and direction:
-        in1.value = False
-        in2.value = False
-        time.sleep(0.5)
-
-        in1.value = True
-        in2.value = False
-
-        direction = False
-
-        
-
-    if leftStop.value and not direction:
-        in1.value = False
-        in2.value = False
-        time.sleep(0.5)
-
-        in1.value = False
-        in2.value = True
-
-        direction = True
-
-       
-    time.sleep(0.01)
+    # x = accel_value(x_axis)
+    # y = accel_value(y_axis)
+    z = accel_value(z_axis)
+    # print('Acceleration (G): ({0})'.format(z))
+    print(z_axis.value)
+    time.sleep(.1)
